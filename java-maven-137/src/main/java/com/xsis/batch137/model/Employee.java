94a16a72f9a86df86ec3ed8014f11bced7fdf78e
Yusuf Fahmi Adiputera <yfa.adiputera@gmail.com>
2018-03-12 13:43:31 +0700

    137

---
+++ java-maven-137/src/main/java/com/xsis/batch137/model/Employee.java
@@ -0,0 +1,69 @@
+package com.xsis.batch137.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Columns;
+
+@Entity
+@Table(name="EMPLOYEE")
+public class Employee {
+
+	
+	public Employee(int id, String name, String address, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.email = email;
+	}
+	public Employee() {
+		
+	}
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private int id;
+	@Column(name="nama")
+	private String name;
+	@Column(name="alamat")
+	private String address;
+	@Column(nullable=false, length=200)
+	private String email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+}
